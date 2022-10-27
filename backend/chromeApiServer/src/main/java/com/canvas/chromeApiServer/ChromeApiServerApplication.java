1078e36de760517ab826fae00a33ccda6b351742
Francis Kogge <koggefa@gmail.com>
2022-10-27 10:36:27 -0700

    moving code i added to new branch

--- backend/chromeApiServer/src/main/java/com/canvas/chromeApiServer/ChromeApiServerApplication.java
+++ backend/chromeApiServer/src/main/java/com/canvas/chromeApiServer/ChromeApiServerApplication.java
@@ -2,18 +2,12 @@ package com.canvas.chromeApiServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class ChromeApiServerApplication implements CommandLineRunner  {
+public class ChromeApiServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChromeApiServerApplication.class, args);
 	}
 
-
-	@Override
-	public void run(String... args) throws Exception {
-		new ChromeApiController();
-	}
 }
