40f2fc7040ae936c9e3bb1f25f99db903b409d86
Francis Kogge <koggefa@gmail.com>
2022-10-26 21:24:24 -0700

    added backend and gitignore

---
+++ backend/chromeApiServer/src/test/java/com/canvas/chromeApiServer/ChromeApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canvas.chromeApiServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChromeApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
