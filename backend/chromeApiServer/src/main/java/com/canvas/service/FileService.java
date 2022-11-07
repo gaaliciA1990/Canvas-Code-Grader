f8e41a56bb325cecc1f118b20a75bc8d9e158177
Francis Kogge <koggefa@gmail.com>
2022-11-06 20:51:45 -0800

    added deleteDirectory to FileService and a new JSONParsingService class

--- backend/chromeApiServer/src/main/java/com/canvas/service/FileService.java
+++ backend/chromeApiServer/src/main/java/com/canvas/service/FileService.java
@@ -3,10 +3,12 @@ package com.canvas.service;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.apache.commons.io.FileUtils;
 import org.reactivestreams.Publisher;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferUtils;
@@ -44,18 +46,43 @@ public class FileService {
         DataBufferUtils.write(dataBufferFlux, path, WRITE).block();
     }
 
-    public boolean writeFile(MultipartFile file) {
+    public boolean writeFileFromUrl(String url, String filename) {
+        try {
+            FileUtils.copyURLToFile(
+                    new URL(url),
+                    new File(fileDirectory, filename)
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean writeFileFromMultipart(MultipartFile file) {
         if (!file.isEmpty()) {
             try {
                 byte[] bytes = file.getBytes();
                 String fileName = file.getOriginalFilename();
-                FileOutputStream fos = new FileOutputStream(fileName);
+                FileOutputStream fos = new FileOutputStream(
+                        new File(fileDirectory, fileName)
+                );
                 fos.write(bytes);
                 fos.close();
             } catch (IOException e) {
                 e.printStackTrace();
                 return false;
             }
+        }
+        return true;
+    }
+
+    public boolean deleteDirectory() {
+        try {
+            FileUtils.deleteDirectory(new File(fileDirectory));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
         return true;
     }
