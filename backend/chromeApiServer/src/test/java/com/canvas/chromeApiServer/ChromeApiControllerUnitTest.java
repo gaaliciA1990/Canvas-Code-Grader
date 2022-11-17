805105a625479d112f21bce4a99bb5571087a85f
Alicia <garcia.alicia1990@gmail.com>
2022-11-17 00:46:34 -0800

    Updated the single test, still failing due to another dependency requiring mockBean.

--- backend/chromeApiServer/src/test/java/com/canvas/chromeApiServer/ChromeApiControllerUnitTest.java
+++ backend/chromeApiServer/src/test/java/com/canvas/chromeApiServer/ChromeApiControllerUnitTest.java
@@ -1,21 +1,29 @@
 package com.canvas.chromeApiServer;
 
 import com.canvas.dto.CommandOutput;
+import com.canvas.service.CanvasClientService;
+import com.canvas.service.FileService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kotlin.Unit;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -27,6 +35,12 @@ class ChromeApiControllerUnitTest {
      */
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    FileService fileService;
+
+    @MockBean
+    CanvasClientService canvasClientService;
 
     /**
      * Placeholder for action to take before running tests
@@ -50,12 +64,20 @@ class ChromeApiControllerUnitTest {
     @Test
     public void should_return_responseEntity_OK() throws Exception {
         // Set Up
-        String authToken = "Iamastringotken";
+        String authToken = "Iamastringtoken";
         MockMultipartFile multipartFile = new MockMultipartFile("files", "test.txt",
                 "text/plain", "Spring Framework".getBytes());
         String userID = "132546";
         String assignmentID = "cs5461321";
         String courseID = "cpsc5023";
+        byte[] list = new byte[1];
+
+        // mock all responses for called dependencies for fileService and canvasClientService
+        Mockito.when(canvasClientService.fetchFileUnderCourseAssignmentFolder(anyString(), anyString(), anyString(), anyString())).thenReturn(list);
+        Mockito.when(fileService.writeFileFromBytes(anyString(), any(), anyString())).thenReturn(true);
+        Mockito.when(fileService.writeFileFromMultipart(any(), anyString())).thenReturn(true);
+        Mockito.when(fileService.getFileDirectory(anyString())).thenReturn("returned test string");
+        Mockito.when(fileService.deleteDirectory(anyString())).thenReturn(true);
 
         // Act
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.multipart("/evaluate")
