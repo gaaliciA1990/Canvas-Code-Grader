3d65cf0193fa73a30bc6467a72666529d590c56e
Alicia <garcia.alicia1990@gmail.com>
2022-11-23 15:09:39 -0800

    Implemented passing test for wrong UserType in initiateStudentCodeEvaluation

--- backend/chromeApiServer/src/test/java/com/canvas/controllers/chromeApiServer/ChromeApiControllerUnitTest.java
+++ backend/chromeApiServer/src/test/java/com/canvas/controllers/chromeApiServer/ChromeApiControllerUnitTest.java
@@ -7,8 +7,11 @@ import com.canvas.service.models.ExtensionUser;
 import com.canvas.service.models.UserType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -23,7 +26,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -64,10 +68,10 @@ class ChromeApiControllerUnitTest {
     }
 
     /**
-     * Tests ResponseEntity returns OK with params
+     * Tests ResponseEntity returns OK with correct params
      */
     @Test
-    public void should_return_responseEntity_OK() throws Exception {
+    public void initiateStudentCodeEvaluation_should_return_responseEntity_OK() throws Exception {
         // Set Up
         String authToken = "Iamastringtoken";
         MockMultipartFile multipartFile = new MockMultipartFile("files", "test.txt",
@@ -99,5 +103,38 @@ class ChromeApiControllerUnitTest {
         assertTrue(controllerResponse.isSuccess());
     }
 
+    /**
+     * Tests the initiateStudentCodeEvaluation method return Bad_Request for wrong userTypes
+     * @param userType      Enum UserType
+     * @throws Exception    exception 
+     */
+    @ParameterizedTest
+    @CsvFileSource(resources = "/chromeAPI_userType_tests.csv", numLinesToSkip = 1)
+    public void initiateStudentCodeEvaluation_should_badRequest_with_wrong_userType(UserType userType) throws Exception {
+        // Set Up
+        String authToken = "TestStringToken";
+        MockMultipartFile multipartFile = new MockMultipartFile("files", "test.txt",
+                "text/plain", "Spring Framework".getBytes());
+        String userID = "132546";
+        String assignmentID = "cs5461321";
+        String courseID = "cpsc5023";
+        UserType type = userType;
+        CommandOutput commandOutput = new CommandOutput(true, "test");
+
+        // Act
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.multipart("/evaluate")
+                        .file(multipartFile)
+                        .header("Authorization", authToken)
+                        .param("assignmentId", assignmentID)
+                        .param("courseId", courseID)
+                        .param("userType", String.valueOf(type))
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())  // assert we get bad request response
+                .andReturn();
+
+        // Assert
+        assertEquals(HttpStatus.BAD_REQUEST.value(), result.getResponse().getStatus());
+    }
 
 }
