01e9886bf46c339020a62a35a929ae5e80d5261a
Alicia <garcia.alicia1990@gmail.com>
2023-01-06 14:59:18 -0800

    Added missing comments to exception handler classes and error response class.

--- backend/chromeApiServer/src/main/java/com/canvas/exceptions/UserNotAuthorizedException.java
+++ backend/chromeApiServer/src/main/java/com/canvas/exceptions/UserNotAuthorizedException.java
@@ -3,18 +3,30 @@ package com.canvas.exceptions;
 import java.io.Serial;
 
 /**
- * Todo: @Francis, add comments to methods and class. Write tests for your code please
+ * Exception to be thrown with message for unauthorize users
  */
 public class UserNotAuthorizedException extends Exception {
 
+    /**
+     * static variable for serial
+     */
     @Serial
     private static final long serialVersionUID = 1L;
 
-    public UserNotAuthorizedException() {
-        super();
-    }
-
+    /**
+     * Constructor that takes in a message to pass to the base exception class
+     * overriding the constructor of Exception class
+     *
+     * @param message String message for the error
+     */
     public UserNotAuthorizedException(String message) {
         super(message);
+    }
+
+    /**
+     * Constructor that takes no arg, but still overrides the constructor of Exception class
+     */
+    public UserNotAuthorizedException() {
+        super();
     }
 }
