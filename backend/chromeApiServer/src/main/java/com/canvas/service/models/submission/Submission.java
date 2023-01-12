5d443c4e933c8f4f1c3ec6edc35b8d6307d71b63
Francis Kogge <koggefa@gmail.com>
2023-01-12 10:45:08 -0800

    added builder pattern, setters for submissionFiles and submissionDirectory, and JsonIgnore to submissionFileBytes to ignore when building the response json

--- backend/chromeApiServer/src/main/java/com/canvas/service/models/submission/Submission.java
+++ backend/chromeApiServer/src/main/java/com/canvas/service/models/submission/Submission.java
@@ -1,30 +1,24 @@
 package com.canvas.service.models.submission;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Arrays;
+import java.util.Map;
 
+@Builder
 @Getter
-@Setter
 public class Submission {
     private String submissionId;
     private String studentId;
     private String assignmentId;
+
+    @Setter
     private SubmissionFile[] submissionFiles;
+    @Setter
     private String submissionDirectory;
 
-    public Submission(
-            String submissionId,
-            String studentId,
-            String assignmentId,
-            SubmissionFile[] submissionFiles,
-            String submissionDirectory
-    ) {
-        this.submissionId = submissionId;
-        this.studentId = studentId;
-        this.assignmentId = assignmentId;
-        this.submissionFiles = Arrays.copyOf(submissionFiles, submissionFiles.length);
-        this.submissionDirectory = submissionDirectory;
-    }
+    @JsonIgnore
+    private Map<String, byte[]> submissionFileBytes;
 }
