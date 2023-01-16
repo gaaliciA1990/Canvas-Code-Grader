4276be6536e905bd19b4b9b957de7bdd55884a3d
Francis Kogge <koggefa@gmail.com>
2023-01-16 09:23:54 -0800

    move lombok builder to constructor for clarity

--- backend/chromeApiServer/src/main/java/com/canvas/service/models/submission/Submission.java
+++ backend/chromeApiServer/src/main/java/com/canvas/service/models/submission/Submission.java
@@ -10,12 +10,15 @@ import java.util.Map;
 /**
  * Student submission model for the JSON response.
  */
-@Builder
 @Getter
 public class Submission {
-    private String submissionId;
-    private String studentId;
-    private String assignmentId;
+    private final String submissionId;
+    private final String studentId;
+    private final String assignmentId;
+
+    // Ignore this field in the response body
+    @JsonIgnore
+    private final Map<String, byte[]> submissionFileBytes;
 
     // Adding setters for these fields so they can be set later
     @Setter
@@ -23,7 +26,11 @@ public class Submission {
     @Setter
     private String submissionDirectory;
 
-    // Ignore this field in the response body
-    @JsonIgnore
-    private Map<String, byte[]> submissionFileBytes;
+    @Builder
+    public Submission(String submissionId, String studentId, String assignmentId, Map<String, byte[]> submissionFileBytes) {
+        this.submissionId = submissionId;
+        this.studentId = studentId;
+        this.assignmentId = assignmentId;
+        this.submissionFileBytes = submissionFileBytes;
+    }
 }
