7771a4877647dd343abb525a5e99842e3f4da457
Francis Kogge <koggefa@gmail.com>
2023-01-21 17:35:42 -0800

    added get file submission call to instructor inject script

--- frontend/canvas-chrome-ext/src/scripts/InstructorInjectScript.js
+++ frontend/canvas-chrome-ext/src/scripts/InstructorInjectScript.js
@@ -1,6 +1,24 @@
 console.log("inside InstructorInjectScript.js")
 
-fetch(chrome.runtime.getURL('components/submission_read_only_view.html')).then(r => r.text()).then(html => {
-    document.body.insertAdjacentHTML('beforeend', html);
-    // not using innerHTML as it would break js event listeners of the page
+let endpoint = "http://localhost:8080/submission/courses/5660191/assignments/33673192/?studentId=36496549&userType=GRADER"
+let bearerToken = "Bearer 7~cQ7XoNd23PrQhB5XBAp8v9osuQsPnyQVsDsZcHb7oTjgnoWYh2lU5qg5RMRMN8rr";
+
+fetch(
+    endpoint, {
+    method: "GET",
+    headers: new Headers({
+        'Authorization': bearerToken
+    })
+}
+).catch(console.error).then(async response => {
+    //console.log(response);
+    const serv_response = await response.text();
+    //console.log(serv_response)
+    console.log(serv_response);
+
+    chrome.runtime.sendMessage({ "message": serv_response });
+
+    alert("sent message to background");
+
 });
+
