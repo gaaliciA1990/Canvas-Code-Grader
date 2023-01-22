8ee6471f5072d5add0f74f41b84c7742c2a6b696
Francis Kogge <koggefa@gmail.com>
2023-01-22 15:47:22 -0800

    pull params for API from canvas url

--- frontend/canvas-chrome-ext/src/scripts/InstructorInjectScript.js
+++ frontend/canvas-chrome-ext/src/scripts/InstructorInjectScript.js
@@ -1,7 +1,36 @@
 console.log("inside InstructorInjectScript.js")
 
-let endpoint = "http://localhost:8080/submission/courses/5660191/assignments/33673192/?studentId=36496549&userType=GRADER"
-let bearerToken = "Bearer 7~cQ7XoNd23PrQhB5XBAp8v9osuQsPnyQVsDsZcHb7oTjgnoWYh2lU5qg5RMRMN8rr";
+
+let bearerToken = "Bearer 7~c5V3FHLUmCwn8II4CvwhMOqZ5HLjxwRt8mVZIspclX9hzlSx6aHg493QMtYidwXp";
+
+const params = (() => {
+    let canvasUrl = window.location.href;
+    console.log(canvasUrl);
+
+    const urlSearchParamsObj = new URLSearchParams(window.location.search);
+    const queryParams = Object.fromEntries(urlSearchParamsObj.entries());
+
+    console.log('query params: ' + queryParams);
+
+    let urlParts = canvasUrl.split('/');
+    console.log(urlParts);
+
+    let params = {};
+
+    for (var i = 0; i < urlParts.length; i++) {
+        if (urlParts[i] === "courses") {
+            params["courseId"] = urlParts[i + 1];
+        }
+    }
+
+    params["assignmentId"] = queryParams["assignment_id"]
+    params["studentId"] = queryParams["student_id"]
+    console.log(params)
+
+    return params;
+})();
+
+let endpoint = `http://localhost:8080/submission/courses/${params["courseId"]}/assignments/${params["assignmentId"]}/?studentId=${params["studentId"]}&userType=GRADER`
 
 fetch(
     endpoint, {
@@ -9,8 +38,7 @@ fetch(
     headers: new Headers({
         'Authorization': bearerToken
     })
-}
-).catch(console.error).then(async response => {
+}).catch(console.error).then(async response => {
     //console.log(response);
     const serv_response = await response.text();
     //console.log(serv_response)
